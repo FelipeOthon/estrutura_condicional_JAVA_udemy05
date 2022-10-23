2797576aef3bafbc8aeeb7d8a2ec79b8d6a7e3ee
FelipeOthon <felipeosantiago@gmail.com>
2022-10-23 14:06:59 -0300

    enviar exercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        double a, r, pi = 3.14159;
+
+        System.out.println("Digite o valor do raio: ");
+        r = scanner.nextDouble();
+
+        a = (pi * (r * r));
+
+        System.out.printf("%.4f%n: ", a);
+
+
+        scanner.close();
     }
 }
